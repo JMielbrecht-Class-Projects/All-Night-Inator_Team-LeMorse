3bc1371808f2f6939e6d14b823ee60905caafabc
Jordan Mielbrecht <mielbrechtj@spu.edu>
2020-03-03 16:36:01 -0800

    Updated UI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,3 +13,18 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_lightButton_clicked()
+{
+
+}
+
+void MainWindow::on_coffeeButton_clicked()
+{
+
+}
+
+void MainWindow::on_blueButton_clicked()
+{
+
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QTime>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -15,7 +15,15 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_lightButton_clicked();
+
+    void on_coffeeButton_clicked();
+
+    void on_blueButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    QTime duration;
 };
 #endif // MAINWINDOW_H
