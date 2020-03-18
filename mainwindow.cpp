ff43c716ca8f4806f956d84db5dbacc3afe63804
Austin White <robotmaker42@gmail.com>
2020-03-18 12:10:54 -0700

    Initial implementation of iot integration
    Pressing the coffee or lights button will send a get request to
    ifttt which then turns on the corisponding smart outlet.
    
    Bugs, errors, things to fix: The returned json is not important
    	but I haven't been able to find a way to properly print it
    	to debug console.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,19 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "httpmanager.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow),
+      webManager(new HttpManager)
 {
     ui->setupUi(this);
+
+    connect(webManager, SIGNAL(CoffeeJsonReady(QJsonObject *)),
+            this, SLOT(processCoffeeJson(QJsonObject *)));
+
+    connect(webManager, SIGNAL(LightsJsonReady(QJsonObject *)),
+            this, SLOT(processLightsJson(QJsonObject *)));
 }
 
 MainWindow::~MainWindow()
@@ -16,15 +24,29 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_lightButton_clicked()
 {
-
+    webManager->sendLightsRequest();
 }
 
 void MainWindow::on_coffeeButton_clicked()
 {
-
+    webManager->sendCoffeeRequest();
 }
 
 void MainWindow::on_blueButton_clicked()
 {
 
 }
+
+
+void MainWindow::processCoffeeJson(QJsonObject *json)
+{
+    qDebug() << "Coffee Json ready";
+    qDebug() << json;
+}
+
+void MainWindow::processLightsJson(QJsonObject *json)
+{
+    qDebug() << "Lights Json ready";
+    qDebug() << json;
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QTime>
+#include "httpmanager.h"
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -16,6 +17,10 @@ public:
     ~MainWindow();
 
 private slots:
+    void processCoffeeJson(QJsonObject *json);
+
+    void processLightsJson(QJsonObject *json);
+
     void on_lightButton_clicked();
 
     void on_coffeeButton_clicked();
@@ -25,5 +30,7 @@ private slots:
 private:
     Ui::MainWindow *ui;
     QTime duration;
+    HttpManager *webManager;
+
 };
 #endif // MAINWINDOW_H
